5f6e789db196fbc171c8ce95bcc7333e1b41fad3
FanaHOVA <fanahova@gmail.com>
2014-09-19 16:58:22 +0200

    0.2 update
    The script can now fetch the decks from the top 10 hot posts and check
    what class it is; the data is then stored in a json file.

--- hs12wins.py
+++ hs12wins.py
@@ -1,57 +1,101 @@
 import praw #Reddit API
+import json
 
-user_agent = ("HS 12 Wins Stats 0.1 by /u/FanaHOVA "
+user_agent = ("HS 12 Wins Stats 0.2 by /u/FanaHOVA "
 			   "github.com/FanaHOVA/HS12WinsLog")
 
 r = praw.Reddit(user_agent=user_agent)
-submissions = r.get_subreddit('12winArenaLog').get_hot(limit=10)
+submissions = r.get_subreddit('12winArenaLog').get_hot(limit=10) #Replace with get_top(limit=3000) to run full script and not just test it
 
-'''PRAW/Reddit API works on lazy objects, splitting the functions to avoid useless data fetching'''
+f = open("database.json","r+")
+db = json.load(f)
 
-def getdeck():
 
-	for each in submissions:
-		deck = []
-		op = each.selftext.lower() #Deck list, need formatting
-		for line in op.split("\n"):
-			deck.append(line)
+for each in submissions:
 
-def getclass():
+	if "meta" in title:
+		pass #Disregard meta posts 
 
-	for each in submissions:
-		title = each.title.lower()
-		
-		if "paladin" in title:
-			return "Paladin"
+	deck = [] #Empty list to fill
+	
+	op = each.selftext.lower() #Whole post from subreddit
 
-		elif "mage" in title:
-			return "Mage"
+	for line in op.split("\n"): #Split each line to analyze if it's a card or not
+		if line.isspace(): #Skip empty lines
+			pass
+		if len(line) > 30: #No card name is that long, probably a url or a note about the deck
+			pass
+		elif len(line) < 5: #No card name is that short
+			pass
+		elif "!" in line or "?" in line or "http" in line: #Skips imgur screens and "Decklist:", "12 wins baby!" etc
+			pass 
+		elif "prizes" in line or "rewards" in line: #Prizes are listed at the end of the post after the deck, you can break from here
+			break    
+		else: #If it passes all the checks, add to the decklist
+			split = line.split(":")
+			card = line[0]
+			copies = line[1]
+			for i in range(copies): #Add
+				deck.append(card)
 
-		elif "warlock" in title:
-			if "zoo" in title:
-				return "Zoolock"
-			elif "hand" in title:
-				return "Handlock"
-			else:
-				return "Warlock"
-		
-		elif "priest" in title:
-			return "Priest"
+	title = each.title.lower() #Title of the post, usually is: [W-L] Class [DD-MM-YYYY]
 
-		elif "hunter" in title:
-			return "Hunter"
+	race = "" #Used to create deck ID
 
-		elif "warrior" in title:
-			return "Warrior"
+	if "paladin" in title:
+		db["classes"]["count"]["Paladin"] += 1
+		race = "Paladin"
 
-		elif "rogue" in title:
-			if "miracle" in title:
-				return "Miracle Rogue"
-			else:
-				return "Rogue"
+	elif "mage" in title:
+		db["classes"]["count"]["Mage"] += 1
+		race = "Mage"
 
-		elif "shaman" in title:
-			return "Shaman"
+	elif "warlock" in title:
+		if "zoo" in title:
+			db["classes"]["count"]["Warlock"]["Zoolock"] += 1
+			race = "Zoolock"
+					
+		elif "hand" in title:
+			db["classes"]["count"]["Warlock"]["Handlock"] += 1
+			race = "Handlock"
 
-		elif "druid" in title:
-			return "Druid"
+		else:
+			db["classes"]["count"]["Warlock"]["Warlock"] += 1
+			race = "Warlock"
+
+	elif "priest" in title:
+		db["classes"]["count"]["Priest"] += 1
+		race = "Priest"
+
+	elif "hunter" in title:
+		db["classes"]["count"]["Hunter"] += 1
+		race = "Hunter"
+
+	elif "warrior" in title:
+		db["classes"]["count"]["Warrior"] += 1
+		race = "Warrior"
+
+	elif "rogue" in title:
+		if "miracle" in title:
+			db["classes"]["count"]["Rogue"]["Miracle"] += 1
+			race = "Miracle"
+
+		else:
+			db["classes"]["count"]["Rogue"]["Rogue"] += 1
+			race = "Rogue"
+
+	elif "shaman" in title:
+			db["classes"]["count"]["Shaman"] += 1
+			race = "Shaman"
+
+	elif "druid" in title:
+			db["classes"]["count"]["Druid"] += 1
+			race = "Druid"
+
+	score = title[0:6]
+	deckid = score + race + "(%s)" % (each.name) #ID of Reddit post
+
+	db["decks"][deckid] = deck
+
+f.seek(0) #Reset file position to the beginning.
+json.dump(db, f, indent=4)
